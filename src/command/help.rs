a96ff5136fcf7b3ad47edb20a4961265fe38166b
Ivo Wetzel <ivo.wetzel@googlemail.com>
2017-05-17 21:24:17 +0200

    Update help text to include streamer watch commands.

--- src/command/help.rs
+++ src/command/help.rs
@@ -13,14 +13,16 @@ static HELP_TEXT_ONE: &'static str = "
 - `!delete <effect>` - Deletes the specified sound effect.
 - `!rename <old_effect_name> <new_effect_name>` - Renames the specified sound effect.
 - `!silence` - Immediately stops all playing sounds and removes all other queued effects.
-- `!greeting <add|remove> <user#ident> [<effect_name>]` - Adds or remove a custom greeting for a user.
-- `!greetings` - Show a list of all existing custom user greetings in a private channel.
-- `!uploader <add|remove> <user#ident> [<effect_name>]` - Adds or remove a user from the list of uploaders for the current server.
-- `!uploaders` - Show a list of all users which are whitelisted to upload sound effects to the current server.
 - `!alias <add|remove> <name> [<effect_name>, ...]` - Adds or remove a effect alias.
 - `!aliases` - Show a list of all existing effect aliases in a private channel.
 - `!bans` - Show a list of all banned users for the current server.
 - `!ban <add|remove> <user#ident>` - Add or remove user bans.
+- `!greeting <add|remove> <user#ident> [<effect_name>]` - Adds or remove a custom greeting for a user.
+- `!greetings` - Show a list of all existing custom user greetings in a private channel.
+- `!uploader <add|remove> <user#ident> [<effect_name>]` - Adds or remove a user from the list of uploaders for the current server.
+- `!uploaders` - Show a list of all users which are whitelisted to upload sound effects to the current server.
+- `!streamer <add|remove> <twitch_nick> [<channel_name>]` - Adds or removes or twitch streamer from the watch list for the specified channel.
+- `!streamers` - Show a list of all watched twitch streamers on the current server..
 - `!leave` - Makes the bot leave its current voice channel.
 - `!pin` - Pins the bot to its current voice channel.
 - `!ip` - Posts the bot's the public IP onto the current channel.
