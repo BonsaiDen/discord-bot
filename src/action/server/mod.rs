ecbdb2c3b2f74d5358f7c2efdf51eaa8998075ca
Ivo Wetzel <ivo.wetzel@googlemail.com>
2017-05-20 21:31:14 +0200

    Remove leave action.

--- src/action/server/mod.rs
+++ src/action/server/mod.rs
@@ -3,7 +3,6 @@ mod download_flac;
 mod download_transcript;
 mod leave_voice;
 mod pin_voice;
-mod reload;
 
 
 // Re-Exports -----------------------------------------------------------------
@@ -11,5 +10,4 @@ pub use self::download_flac::Action as DownloadFlac;
 pub use self::download_transcript::Action as DownloadTranscript;
 pub use self::leave_voice::Action as LeaveVoice;
 pub use self::pin_voice::Action as PinVoice;
-pub use self::reload::Action as Reload;
 
